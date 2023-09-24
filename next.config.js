587c8375b3b5d1520bded8c7c1a249da1c3661b3
Christian Hackl <chack@live.at>
2023-09-24 12:19:54 +0200

    fix location

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/scrumpoker",
+  //basePath: "/app/scrumpoker",
 }
 
 module.exports = nextConfig
