75ca65b9b3f1f27b2d8a45931029dbe7d5e07845
Christian Hackl <chack@live.at>
2023-09-24 13:06:11 +0200

    fix location

--- components/layout.tsx
+++ components/layout.tsx
@@ -15,12 +15,12 @@ export default function Layout({children, title = ""}: LayoutParam) {
       <Head>
         <title>Scrumpoker {title}</title>
         <meta name="description" content="Scrumpoker" />
-        <link rel="icon" href="/scrumpoker/favicon/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/scrumpoker/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/scrumpoker/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/scrumpoker/favicon/favicon-16x16.png" />
-        <link rel="manifest" href="/scrumpoker/favicon/site.webmanifest" />
-        <link rel="mask-icon" href="/scrumpoker/favicon/safari-pinned-tab.svg" color="#33050d" />
+        <link rel="icon" href="./favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png" />
+        <link rel="manifest" href="./favicon/site.webmanifest" />
+        <link rel="mask-icon" href="./favicon/safari-pinned-tab.svg" color="#33050d" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#272727" />
       </Head>
